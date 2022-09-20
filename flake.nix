b342b1d213b57a39fd792e5fb8bfbeb817603854
Kristoffer K. Føllesdal <kristoffer.follesdal@eviny.no>
2022-09-20 17:45:13 +0200

    flake: Use bkkp/flake-registry

--- flake.nix
+++ flake.nix
@@ -1,5 +1,10 @@
 {
-  outputs = { self, ... }@inputs: {
+  nixConfig.flake-registry = "https://raw.githubusercontent.com/bkkp/flake-registry/main/flake-registry.json";
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-22.05";
+
+  outputs = { self, nixpkgs }@inputs:
+  {
     nixosModules = import ./hardware;
   };
 }
