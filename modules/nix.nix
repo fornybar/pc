a67796d960561e86057cc2370d2def22d2300aea
Kristoffer K. Føllesdal <kristoffer.follesdal@eviny.no>
2022-09-23 18:39:44 +0200

    Add moduel for nix with tests

---
+++ modules/nix.nix
@@ -0,0 +1,10 @@
+{ lib, ... }@inputs:
+with lib;
+{
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+  };
+
+  # If nix is a input use nix version from it
+  nixpkgs.overlays = optional (inputs ? "nix") inputs.nix.overlays.default;
+}
