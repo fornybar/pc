4624904132ccf6426b41082b6b91b5b5bb8665cf
Kristoffer K. Føllesdal <kristoffer.follesdal@eviny.no>
2022-10-01 15:01:52 +0200

    Add sops config

---
+++ modules/sops.nix
@@ -0,0 +1,17 @@
+{ self, pkgs, lib, sops-nix, users, ... }:
+{
+
+  imports = [ 
+    sops-nix.nixosModules.sops
+  ];
+
+  environment.systemPackages = [ pkgs.sops ];
+
+  sops.age.keyFile = "/root/.config/sops/age/keys.txt";
+  sops.defaultSopsFile = "${self}/secrets.yaml";
+
+  sops.secrets = lib.mkMerge (map (name:{
+      "${name}/password".neededForUsers = true;
+  }) users);
+
+}
