a26bcbb949d74ba5ce98e740ebe237e8078ca9c3
Kristoffer K. Føllesdal <kristoffer.follesdal@eviny.no>
2022-10-02 12:05:29 +0200

    Add default nixosModules
    Collection of a good modules to start with

--- flake.nix
+++ flake.nix
@@ -6,7 +6,23 @@
 
   outputs = { self, nixpkgs, nix, sops-nix, utils }@inputs:
   {
-    nixosModules = import ./hardware // utils.lib.importDir ./modules;
+    nixosModules = import ./hardware // utils.lib.importDir ./modules
+    // {
+      default = {
+        imports = [
+          self.nixosModules.nix
+          self.nixosModules.users
+          self.nixosModules.system
+          self.nixosModules.fileSystems
+          self.nixosModules.keyboard
+          self.nixosModules.sops
+          self.nixosModules.desktop
+          self.nixosModules.virtualisation
+          self.nixosModules.home-manager
+        ];
+      };
+    };
+
 
     checks."x86_64-linux" = import ./tests inputs;
   };
