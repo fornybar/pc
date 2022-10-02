284d5b6b1723e8df64a459a81f16c9e566225f76
Kristoffer K. Føllesdal <kristoffer.follesdal@eviny.no>
2022-10-02 22:18:14 +0200

    Add home-manager config module

---
+++ modules/home-manager.nix
@@ -0,0 +1,51 @@
+{ config, lib, home-manager, mapUsers, ... }:
+with lib;
+
+let
+  cfg = config.midgard.pc.users;
+
+  mapHomeManagerUsers = attrsFun: mapUsers (name: user:
+    mkIf cfg."${name}".home-manager.enable (attrsFun name user)
+  );
+
+  userOpts = {
+    options.home-manager = mkOption {
+      type = with types; submodule {
+        options = {
+          enable = mkOption {
+            type = types.bool;
+            example = true;
+            default = true;
+            description = "Enable home-manager";
+          };
+        };
+      };
+      default = { };
+    };
+  };
+in {
+
+  imports = [ 
+    home-manager.nixosModules.home-manager
+  ];
+
+  options = {
+    midgard.pc.users = mkOption {
+      type = with types; attrsOf (submodule userOpts);
+    };
+  };
+
+  config = {
+    _module.args = { inherit mapHomeManagerUsers; };
+
+    home-manager.useGlobalPkgs = true;
+    home-manager.useUserPackages = true;
+
+    home-manager.users = mapHomeManagerUsers (name: user: {
+      home.stateVersion = "22.05";
+
+      home.username = user.name;
+      home.homeDirectory = config.users.users."${name}".home;
+    });
+  };
+}
