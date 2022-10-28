6361f53f158088831c0397938b13c9949b817aaa
kfollesdal <kristoffer.follesdal@eviny.no>
2022-10-28 17:30:57 +0200

    Add first version of nixos config template

--- flake.nix
+++ flake.nix
@@ -35,6 +35,15 @@
 
     checks."x86_64-linux" = import ./tests inputs;
 
+    templates.default = {
+      path = ./templates;
+      description = "Example setup for nixos pc";
+      welcomeText = ''
+      # Nixos config setup
+      Setup a simple nixos pc
+      ''; # Can use markdown here
+    };
+
     devShells."x86_64-linux".default = let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
     in
