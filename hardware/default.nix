293b6298dc9f90349a0b86010b96498955ee6695
kfollesdal <kristoffer.follesdal@eviny.no>
2022-10-31 13:06:07 +0100

    Get paths not import hardware

--- hardware/default.nix
+++ hardware/default.nix
@@ -1,4 +1,4 @@
 {
-  hdw-hp-elitebook-830-g5 = import ./hp/elitebook/830_G5.nix;
-  hdw-hp-zbook-firefly_15_g8 = import ./hp/zbook/firefly_15_G8.nix;
+  hdw-hp-elitebook-830-g5 = ./hp/elitebook/830_G5.nix;
+  hdw-hp-zbook-firefly_15_g8 = ./hp/zbook/firefly_15_G8.nix;
 }
