2be84c2836c40432982f28bf2724dde1cf319494
Kristoffer K. Føllesdal <kristoffer.follesdal@eviny.no>
2023-07-31 16:42:43 +0200

    Add hardware config for Thinkpad T14s

--- hardware/default.nix
+++ hardware/default.nix
@@ -1,4 +1,5 @@
 {
   hdw-hp-elitebook-830-g5 = import ./hp/elitebook/830_G5.nix;
   hdw-hp-zbook-firefly_15_g8 = import ./hp/zbook/firefly_15_G8.nix;
+  hdw-lenovo-t14s-intel-gen3 = import ./lenovo/thinkpad/t14s/intel-gen3.nix;
 }
