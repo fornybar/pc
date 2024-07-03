2a0235dc115a2752ca4ae78888c5ea58acdf301b
masve123 <101531168+masve123@users.noreply.github.com>
2024-07-03 16:05:12 +0200

    Add hardware config for HP Zbook Firefly 16 G11 (#27)

--- hardware/default.nix
+++ hardware/default.nix
@@ -1,6 +1,7 @@
 {
   hdw-hp-elitebook-830-g5 = import ./hp/elitebook/830_G5.nix;
   hdw-hp-zbook-firefly_15_g8 = import ./hp/zbook/firefly_15_G8.nix;
+  hdw-hp-zbook-firefly_16_g11 = import ./hp/zbook/firefly_16_G11.nix;
   hdw-lenovo-t14s-intel-gen3 = import ./lenovo/thinkpad/t14s/intel-gen3.nix;
   hdw-orbstack = import ./orbstack.nix;
 }
