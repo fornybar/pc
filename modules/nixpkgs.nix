b7a392cf13d4b7c54bb056f31fa3d5331039ccde
kfollesdal <kristoffer.follesdal@eviny.no>
2024-07-12 12:37:49 +0200

    Do not look nixpkgs registry entry to system installations (#28)
    See paragraph 4 in:
    https://nixos.org/manual/nixos/stable/release-notes#sec-release-24.05-highlights

--- modules/nixpkgs.nix
+++ modules/nixpkgs.nix
@@ -1,3 +1,10 @@
 {
   nixpkgs.config.allowUnfree = true;
+
+  # Solve that nixpkgs is looked to system installation
+  # See paragraph 4 in https://nixos.org/manual/nixos/stable/release-notes#sec-release-24.05-highlights
+  nixpkgs.flake = {
+    setFlakeRegistry = false;
+    setNixPath = false;
+  };
 }
