dce3652ad1c12b3b8703628149c4991695a05acc
harvidsen <hakon.arvidsen@eviny.no>
2024-08-19 17:01:33 +0200

    hp(firefly): Make nvidia more robust

--- hardware/hp/zbook/firefly_G11.nix
+++ hardware/hp/zbook/firefly_G11.nix
@@ -14,4 +14,26 @@
   networking.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+  # Make nvidia less error prone (https://nixos.wiki/wiki/Nvidia)
+  hardware.nvidia = {
+    open = lib.mkDefault false;
+    nvidiaSettings = lib.mkDefault true; # For debugging
+
+    # nvidiaPackages.stable didn't build on nixos-24.05 so use newer version
+    # However, we need to change it back when stable works again.
+    package = lib.mkDefault (config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "555.58.02";
+      sha256_64bit = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
+      sha256_aarch64 = "sha256-wb20isMrRg8PeQBU96lWJzBMkjfySAUaqt4EgZnhyF8=";
+      openSha256 = "sha256-8hyRiGB+m2hL3c9MDA/Pon+Xl6E788MZ50WrrAGUVuY=";
+      settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+      persistencedSha256 = "sha256-a1D7ZZmcKFWfPjjH1REqPM5j/YLWKnbkP9qfRyIyxAw=";
+    });
+
+    powerManagement = {
+      enable = lib.mkDefault false;
+      finegrained = lib.mkDefault false;
+    };
+  };
 }
