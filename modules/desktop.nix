ff5e048195e69b86eabef8c49d94c9405c35edd3
andershus <anders.husebo@eviny.no>
2025-08-25 12:16:45 +0200

    dektop: format

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -1,50 +1,68 @@
-{ config, pkgs, lib, mapHomeManagerUsers, ...}:
+{
+  config,
+  pkgs,
+  lib,
+  mapHomeManagerUsers,
+  ...
+}:
 with lib;
 let
   cfg = config.midgard.pc;
-in {
+in
+{
   options.midgard.pc = {
     desktop = mkOption {
-      type = with types; nullOr (enum [ "gnome" "plasma" "sway" ]);
+      type =
+        with types;
+        nullOr (enum [
+          "gnome"
+          "plasma"
+          "sway"
+        ]);
       default = "gnome";
       description = ''Which dekstop to use "gnome", "plasma" or null'';
     };
   };
 
-  config = mkIf (cfg.desktop != null) (
-    mkMerge [
-      { services.xserver.enable = true; }
+  config = mkIf (cfg.desktop != null) (mkMerge [
+    { services.xserver.enable = true; }
 
-      (mkIf (cfg.desktop == "gnome") {
-        services.xserver = {
-          desktopManager.gnome.enable = true;
-          displayManager.gdm.enable = true;
-        };
+    (mkIf (cfg.desktop == "gnome") {
+      services.xserver = {
+        desktopManager.gnome.enable = true;
+        displayManager.gdm.enable = true;
+      };
 
-        # Remove unused programs
-        environment.gnome.excludePackages = (with pkgs; [
+      # Remove unused programs
+      environment.gnome.excludePackages = (
+        with pkgs;
+        [
           epiphany # webbrowser use firefox
           geary # email reader
-        ]);
-      })
+        ]
+      );
+    })
 
-      (mkIf (cfg.desktop == "plasma") {
-        services.xserver = {
-          displayManager.sddm.enable = true;
-          desktopManager.plasma5.enable = true;
-        };
-      })
+    (mkIf (cfg.desktop == "plasma") {
+      services.xserver = {
+        displayManager.sddm.enable = true;
+        desktopManager.plasma5.enable = true;
+      };
+    })
 
-      (mkIf (cfg.desktop == "sway") {
-        security.polkit.enable = true;
-        security.pam.services.swaylock = {};
-        programs.light.enable = true;
+    (mkIf (cfg.desktop == "sway") {
+      security.polkit.enable = true;
+      security.pam.services.swaylock = { };
+      programs.light.enable = true;
 
-        users.users = mapHomeManagerUsers (name: user: {
+      users.users = mapHomeManagerUsers (
+        name: user: {
           extraGroups = [ "video" ];
-        });
-        environment.sessionVariables.GTK_USE_PORTAL = "1";
-        home-manager.users = mapHomeManagerUsers (name: user: {
+        }
+      );
+      environment.sessionVariables.GTK_USE_PORTAL = "1";
+      home-manager.users = mapHomeManagerUsers (
+        name: user: {
           wayland.windowManager.sway = {
             enable = true;
             config = rec {
@@ -65,11 +83,12 @@ in {
               enable = true;
             };
           };
-        });
+        }
+      );
 
-        services.greetd = {
-          enable = true;
-          settings = {
+      services.greetd = {
+        enable = true;
+        settings = {
           default_session.command = ''
             ${pkgs.greetd.tuigreet}/bin/tuigreet \
               --time \
@@ -77,15 +96,14 @@ in {
               --user-menu \
               --cmd sway
           '';
-          };
         };
+      };
 
-        environment.etc."greetd/environments".text = ''
-          sway
-        '';
-      })
+      environment.etc."greetd/environments".text = ''
+        sway
+      '';
+    })
 
-    ]
-  );
+  ]);
 
 }
