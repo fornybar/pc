b29758ccef268bb85bef3aed4dbc900b12fece2d
HåkonIA <62279738+harvidsen@users.noreply.github.com>
2025-12-19 12:16:36 +0100

    Rename settings for nixos 25.11 (#41)
    Includes formatting

--- modules/home-manager-git.nix
+++ modules/home-manager-git.nix
@@ -1,39 +1,52 @@
-{ config, lib, mapHomeManagerUsers, ... }:
+{
+  config,
+  lib,
+  mapHomeManagerUsers,
+  ...
+}:
 with lib;
 
 let
   cfg = config.midgard.pc.users;
 
-  gitOpts = { name, ...}: {
-    options.git = {
-      enable = mkOption {
-        type = types.bool;
-        default = true;
+  gitOpts =
+    { name, ... }:
+    {
+      options.git = {
+        enable = mkOption {
+          type = types.bool;
+          default = true;
+        };
+
+        userName = mkOption {
+          type = types.str;
+          description = "Your GitHub username";
+        };
+
+        githubTokenPath = mkOption {
+          type = with types; nullOr path;
+          description = "GitHub personal token";
+        };
+
+        editor = mkOption {
+          type =
+            with types;
+            enum [
+              "nano"
+              "vim"
+            ];
+          description = "Default git text editor";
+          default = "nano";
+        };
       };
 
-      userName = mkOption {
-        type = types.str;
-        description = "Your GitHub username";
-      };
-
-      githubTokenPath = mkOption {
-        type = with types; nullOr path;
-        description = "GitHub personal token";
-      };
-
-      editor = mkOption {
-        type = with types; enum [ "nano" "vim" ];
-        description = "Default git text editor";
-        default = "nano";
+      config = {
+        git.githubTokenPath = mkDefault (config.sops.secrets."${name}/github-token".path or null);
       };
     };
 
-    config = {
-      git.githubTokenPath = mkDefault (config.sops.secrets."${name}/github-token".path or null);
-    };
-  };
-
-in {
+in
+{
 
   options = {
     midgard.pc.users = mkOption {
@@ -44,36 +57,39 @@ in {
   config = {
     assertions = [
       {
-          assertion = ! any id (lib.mapAttrsToList (name: user: isNull user.git.githubTokenPath) cfg);
-          message = ''
-            Sops secret <user>:github.token is missing from secrets.yaml for one or more users. Set it og change
-            midgard.pc.users.<users>.git.githubTokenPath to a file with github-token.
-          '';
+        assertion = !any id (lib.mapAttrsToList (name: user: isNull user.git.githubTokenPath) cfg);
+        message = ''
+          Sops secret <user>:github.token is missing from secrets.yaml for one or more users. Set it og change
+          midgard.pc.users.<users>.git.githubTokenPath to a file with github-token.
+        '';
       }
     ];
 
-    home-manager.users = mapHomeManagerUsers (name: user:
-       mkIf cfg.${name}.git.enable {
-         programs = {
-           gh.enable = true;
-           gh.gitCredentialHelper.enable= true;
+    home-manager.users = mapHomeManagerUsers (
+      name: user:
+      mkIf cfg.${name}.git.enable {
+        programs = {
+          gh.enable = true;
+          gh.gitCredentialHelper.enable = true;
 
-           git.enable = true;
-           git.userName = user.git.userName;
-           git.userEmail = user.email;
-           git.extraConfig = {
-             init.defaultBranch = "main";
-             core.editor = cfg.${name}.git.editor;
-             push.autoSetupRemote = true;
-           };
-           #git.signing.key
-           #git signing.signByDefault = true;
-         };
+          git = {
+            enable = true;
+            settings = {
+              user.name = user.git.userName;
+              user.email = user.email;
+              init.defaultBranch = "main";
+              core.editor = cfg.${name}.git.editor;
+              push.autoSetupRemote = true;
+            };
+          };
+          #git.signing.key
+          #git signing.signByDefault = true;
+        };
 
-         home.sessionVariablesExtra = ''
-           export GH_TOKEN=''$(cat ${cfg.${name}.git.githubTokenPath})
-         '';
-       }
+        home.sessionVariablesExtra = ''
+          export GH_TOKEN=''$(cat ${cfg.${name}.git.githubTokenPath})
+        '';
+      }
     );
   };
 }
